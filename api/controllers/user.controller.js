4910fda4f177d0c276c6863763694d0f9c142d83
Shreya Chatterjee <shreyachatterjee095@gmail.com>
2024-03-21 13:11:21 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -56,5 +56,16 @@ try {
     next(error);
   }
 
-}
+};
 
+export const deleteUser = async (req, res, next) => {
+    if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+    } catch (error) {
+      next(error);
+    }
+  };
