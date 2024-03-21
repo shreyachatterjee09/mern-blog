09881628902e21eebf986e7549336bea25b92d91
Shreya Chatterjee <shreyachatterjee095@gmail.com>
2024-03-21 13:02:51 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,14 +15,15 @@ export const updateUser = async (req,res,next) =>{
           return next(errorHandler(400, 'Password must be at least 6 characters'));
         }
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
-}
+
+    }
 if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
         errorHandler(400, 'Username must be between 7 and 20 characters')
       );
     }
-}
+
 if (req.body.username.includes(' ')) {
   return next(errorHandler(400, 'Username cannot contain spaces'));
 }
@@ -33,6 +34,7 @@ if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
   return next(
     errorHandler(400, 'Username can only contain letters and numbers')
   );
+}
 }
 try {
     const updatedUser = await User.findByIdAndUpdate(
@@ -53,6 +55,6 @@ try {
    catch (error) {
     next(error);
   }
-};
 
+}
 
